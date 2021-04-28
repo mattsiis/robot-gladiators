7a382460ed02036590104b689337ef5c294149b3
mattsiis <35969076+mattsiis@users.noreply.github.com>
2021-04-28 00:59:06 -0400

    add first function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alart! JS is running!")
+window.alert("This is an alart! JS is running!");
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+fight();
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
